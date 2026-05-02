9ad2cc4198d347bb30d522f9fdffa877da83aed6
Marcos Henrique de Santana <76234459+marcoshssilva@users.noreply.github.com>
2026-05-02 01:59:11 -0300

    feat: Add shutdown hook for web server and implement startup banner

--- cloud-api-controller-web/src/main/java/com/github/marcoshssilva/cloud/api/controller/web/App.java
+++ cloud-api-controller-web/src/main/java/com/github/marcoshssilva/cloud/api/controller/web/App.java
@@ -1,6 +1,7 @@
 package com.github.marcoshssilva.cloud.api.controller.web;
 
 import com.github.marcoshssilva.cloud.api.controller.web.exceptions.ApplicationStartupErrorException;
+import com.github.marcoshssilva.cloud.api.controller.web.exceptions.WebServerError;
 import com.github.marcoshssilva.cloud.api.controller.web.utils.LoggerHelper;
 import org.apache.logging.log4j.Logger;
 import org.jboss.weld.environment.se.Weld;
@@ -15,6 +16,9 @@ import jakarta.inject.Named;
 @ApplicationScoped
 public class App implements ApplicationRunner {
     public static final Logger logger = LoggerHelper.getLogger(App.class);
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final Thread CURRENT_THREAD = Thread.currentThread();
 
     private final WebServer webServer;
 
@@ -34,10 +38,32 @@ public class App implements ApplicationRunner {
 
     @Override
     public void run(String... args) throws ApplicationStartupErrorException {
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            try {
+                logger.info("Shutting down web server...");
+                webServer.stop();
+            } catch (WebServerError e) {
+                logger.error("Failed to stop web server during shutdown. Cause: {}", e.getMessage(), e);
+            }
+        }));
+        this.printBanner();
         try (WebServer server = webServer.start()) {
-            logger.info("Web server started on port {}", server.getPort());
+            logger.info("Web server started at {} on port {}", server.getHost(), server.getPort());
+            logger.info("Management server started at {} on port {}", server.getManagementHost(), server.getManagementPort());
+            CURRENT_THREAD.join();
         } catch (Exception e) {
             throw new ApplicationStartupErrorException("Failed to start web server", e);
         }
+    }
+
+    public void printBanner() {
+        System.out.println(ANSI_CYAN +
+                "  ____ _                 _      _    ____ ___    ____            _             _ _               \n" +
+                " / ___| | ___  _   _  __| |    / \\  |  _ \\_ _|  / ___|___  _ __ | |_ _ __ ___ | | | ___ _ __ \n" +
+                "| |   | |/ _ \\| | | |/ _` |   / _ \\ | |_) | |  | |   / _ \\| '_ \\| |_| '__/ _ \\| | |/ _ \\ '__|\n" +
+                "| |___| | (_) | |_| | (_| |  / ___ \\|  __/| |  | |__| (_) | | | | |_| | | (_) | | |  __/ |   \n" +
+                " \\____|_|\\___/ \\__,_|\\__,_| /_/   \\_\\_|  |___|  \\____\\___/|_| |_|\\__|_|  \\___/|_|_|\\___|_|   \n" +
+                "" +
+                ANSI_RESET);
     }
 }
