8d38e6a9d41c599303c96abd879bc609c69b03e6
Marcos Henrique de Santana <76234459+marcoshssilva@users.noreply.github.com>
2026-05-03 04:42:50 -0300

    tests: Create tests to check if app startup with health check OK

--- cloud-api-controller-web/src/test/java/com/github/marcoshssilva/cloud/api/controller/web/WebStartupInitializerTests.java
+++ cloud-api-controller-web/src/test/java/com/github/marcoshssilva/cloud/api/controller/web/WebStartupInitializerTests.java
@@ -1,25 +1,35 @@
 package com.github.marcoshssilva.cloud.api.controller.web;
 
+import com.github.marcoshssilva.cloud.api.controller.core.utils.StandaloneStartupProcess;
+import com.github.marcoshssilva.cloud.api.controller.core.utils.WeldContainerHelper;
 import com.github.marcoshssilva.cloud.api.controller.web.data.WebServerStatus;
-import com.github.marcoshssilva.cloud.api.controller.web.impl.HttpRequestProcessorImpl;
 import com.github.marcoshssilva.cloud.api.controller.web.interfaces.WebServer;
-import com.github.marcoshssilva.cloud.api.controller.web.impl.UndertowServerWebServerImpl;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class WebStartupInitializerTests {
 
-    @DisplayName("Should start and stop web server with success")
+    @DisplayName("Should test if app run with health check OK")
     @Test
-    void shouldStartWebServerWithSuccess() throws Exception {
-        try(WebServer webServer = new UndertowServerWebServerImpl(new HttpRequestProcessorImpl())) {
-            assertDoesNotThrow(webServer::start);
-            assertEquals(WebServerStatus.RUNNING, webServer.getStatus());
-            assertDoesNotThrow(webServer::stop);
-            assertEquals(WebServerStatus.STOPPED, webServer.getStatus());
-        }
+    void shouldTestIfAppRunWithHealthCheckOK() throws Exception {
+        StandaloneStartupProcess.main(new String[0]);
+        WebServer server = WeldContainerHelper.getContainer().select(WebServer.class).iterator().next();
+        assertEquals(WebServerStatus.RUNNING, server.getStatus());
+
+        HttpClient client   = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(new java.net.URI("http://localhost:8080/health/status"))
+                .GET()
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        assertEquals(200, response.statusCode());
+        assertEquals("{ \"status\": \"OK\" }", response.body());
     }
 }
