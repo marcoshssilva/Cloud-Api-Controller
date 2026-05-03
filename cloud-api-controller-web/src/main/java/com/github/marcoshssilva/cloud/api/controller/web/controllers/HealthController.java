8db71b589d579bea6eb595f5dd01d5ee4b26cc88
Marcos Henrique de Santana <76234459+marcoshssilva@users.noreply.github.com>
2026-05-03 03:24:06 -0300

    feat: Create default controller HealthController for health status check

---
+++ cloud-api-controller-web/src/main/java/com/github/marcoshssilva/cloud/api/controller/web/controllers/HealthController.java
@@ -0,0 +1,24 @@
+package com.github.marcoshssilva.cloud.api.controller.web.controllers;
+
+import com.github.marcoshssilva.cloud.api.controller.web.data.HttpMethod;
+import com.github.marcoshssilva.cloud.api.controller.web.data.HttpStatusCode;
+import com.github.marcoshssilva.cloud.api.controller.web.impl.HttpResponseImpl;
+import com.github.marcoshssilva.cloud.api.controller.web.interfaces.HttpController;
+import com.github.marcoshssilva.cloud.api.controller.web.interfaces.HttpOperation;
+import com.github.marcoshssilva.cloud.api.controller.web.interfaces.HttpResponse;
+
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+@HttpController(path = "/health")
+public class HealthController {
+    @HttpOperation(method = HttpMethod.GET, path = "/status")
+    public HttpResponse getStatus() {
+        return new HttpResponseImpl(
+                List.of(),
+                List.of(),
+                "{ \"status\": \"OK\" }".getBytes(StandardCharsets.UTF_8),
+                HttpStatusCode.OK
+        );
+    }
+}
