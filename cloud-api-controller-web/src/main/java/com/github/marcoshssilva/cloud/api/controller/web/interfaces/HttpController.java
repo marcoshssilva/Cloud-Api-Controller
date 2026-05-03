07e0acd4d6b849aceea68f87ca8e1f894cc6712e
Marcos Henrique de Santana <76234459+marcoshssilva@users.noreply.github.com>
2026-05-02 22:49:41 -0300

    feat: Update annotations HttpOperation and HttpController

--- cloud-api-controller-web/src/main/java/com/github/marcoshssilva/cloud/api/controller/web/interfaces/HttpController.java
+++ cloud-api-controller-web/src/main/java/com/github/marcoshssilva/cloud/api/controller/web/interfaces/HttpController.java
@@ -1,5 +1,20 @@
 package com.github.marcoshssilva.cloud.api.controller.web.interfaces;
 
+import jakarta.enterprise.context.NormalScope;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Target({ TYPE })
+@Retention(RUNTIME)
+@Documented
+@NormalScope
+@Inherited
 public @interface HttpController {
-    String path();
+    String path() default "/" ;
 }
--- cloud-api-controller-web/src/main/java/com/github/marcoshssilva/cloud/api/controller/web/interfaces/HttpOperation.java
+++ cloud-api-controller-web/src/main/java/com/github/marcoshssilva/cloud/api/controller/web/interfaces/HttpOperation.java
@@ -1,8 +1,22 @@
 package com.github.marcoshssilva.cloud.api.controller.web.interfaces;
 
 import com.github.marcoshssilva.cloud.api.controller.web.data.HttpMethod;
+import jakarta.enterprise.context.NormalScope;
 
+import java.lang.annotation.Documented;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Target({ METHOD })
+@Retention(RUNTIME)
+@Documented
+@NormalScope
+@Inherited
 public @interface HttpOperation {
-    HttpMethod method();
-    String path();
+    HttpMethod method() default HttpMethod.GET;
+    String path() default "";
 }
