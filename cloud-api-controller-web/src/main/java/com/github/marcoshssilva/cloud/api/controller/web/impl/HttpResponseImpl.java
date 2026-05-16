893beea51795f5e78ad42156ba57d50fea02b31b
Marcos Henrique de Santana <76234459+marcoshssilva@users.noreply.github.com>
2026-05-15 23:41:57 -0300

    lint: Remove unused List import from HttpResponseImpl

--- cloud-api-controller-web/src/main/java/com/github/marcoshssilva/cloud/api/controller/web/impl/HttpResponseImpl.java
+++ cloud-api-controller-web/src/main/java/com/github/marcoshssilva/cloud/api/controller/web/impl/HttpResponseImpl.java
@@ -7,7 +7,6 @@ import com.github.marcoshssilva.cloud.api.controller.web.interfaces.HttpResponse
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
 
 public class HttpResponseImpl implements HttpResponse {
     private Collection<HttpHeader> headers = new ArrayList<>(0);
